b337c41c1d11c5bb62b16f2ab90281fe745f00c2
TanwarVipin <130320054+TanwarVipin@users.noreply.github.com>
2023-04-12 01:18:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 00:16:02 2023
+
+@author: user
+"""
+import pickle
+clf=pickle.load(open('model.pkl','rb'))
+import streamlit as st
+def welcome():
+    return 'Welcome All'
+def prediction(sl,sw,pl,pw):
+    pred=clf.predict([[sl,sw,pl,pw]])
+    return pred
+def main():
+    st.title('Iris Flower Prediction')
+    html_temp = """
+    <div style ="background-color:yellow;padding:13px">
+    <h1 style ="color:black;text-align:center;">Iris Flower Classifier ML App </h1>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    sl=st.text_input('sepal_length','')
+    sw=st.text_input('sepal_width','')
+    pl=st.text_input('petal_length','')
+    pw=st.text_input('petal_width','')
+    l=[sl,sw,pl,pw]
+    result=''
+    if st.button('Predict'):
+        result=prediction(sl,sw,pl,pw)
+
+    if result==0:
+        st.success(f'The flower Type is Virginica')
+    elif result==1:
+        st.success(f'The flower Type is Setosa')
+    elif result==2:
+        st.success(f'The Flower Type is Versicolor')
+    
+if __name__=='__main__':
+    main()
+import os 
+os.getcwd()
